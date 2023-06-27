693a233239da05bad69710ae4df1ac50c55d1e03
Alberto <unumnio@gmail.com>
2023-06-26 22:12:40 -0500

    creating Date class with its exceptions

--- ConceptsPOO/Program.cs
+++ ConceptsPOO/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConceptsPOO;
+
+try
+{
+    var date1 = new Date() { Year = 2028, Month = 2, Day = 29 };
+    var date2 = new Date() { Year = 1974, Month = 12, Day = 14 };
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
