1b24a9709fb6018e42f5496fb0f9a3d6315df4bf
Alberto <unumnio@gmail.com>
2023-06-29 16:49:27 -0500

    ConceptsPOO.UserInterface - implement the logic to the interface

--- ConceptsPOO.UserInterface/Form1.Designer.cs
+++ ConceptsPOO.UserInterface/Form1.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtYear = new TextBox();
+            txtMonth = new TextBox();
+            txtDay = new TextBox();
+            btnValidate = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(57, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(32, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Year:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(57, 98);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Month:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(57, 161);
+            label3.Name = "label3";
+            label3.Size = new Size(30, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Day:";
+            // 
+            // txtYear
+            // 
+            txtYear.Location = new Point(135, 44);
+            txtYear.Name = "txtYear";
+            txtYear.PlaceholderText = "Enter the year...";
+            txtYear.Size = new Size(110, 23);
+            txtYear.TabIndex = 3;
+            // 
+            // txtMonth
+            // 
+            txtMonth.Location = new Point(135, 95);
+            txtMonth.Name = "txtMonth";
+            txtMonth.PlaceholderText = "Enter the month...";
+            txtMonth.Size = new Size(110, 23);
+            txtMonth.TabIndex = 4;
+            // 
+            // txtDay
+            // 
+            txtDay.Location = new Point(135, 158);
+            txtDay.Name = "txtDay";
+            txtDay.PlaceholderText = "Enter the day...";
+            txtDay.Size = new Size(110, 23);
+            txtDay.TabIndex = 5;
+            // 
+            // btnValidate
+            // 
+            btnValidate.Location = new Point(57, 218);
+            btnValidate.Name = "btnValidate";
+            btnValidate.Size = new Size(188, 23);
+            btnValidate.TabIndex = 6;
+            btnValidate.Text = "Validate";
+            btnValidate.UseVisualStyleBackColor = true;
+            btnValidate.Click += btnValidate_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(325, 308);
+            Controls.Add(btnValidate);
+            Controls.Add(txtDay);
+            Controls.Add(txtMonth);
+            Controls.Add(txtYear);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Date Validator";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtYear;
+        private TextBox txtMonth;
+        private TextBox txtDay;
+        private Button btnValidate;
     }
 }
--- ConceptsPOO.UserInterface/Form1.cs
+++ ConceptsPOO.UserInterface/Form1.cs
@@ -1,3 +1,5 @@
+using ConceptsPOO.Logic;
+
 namespace ConceptsPOO.UserInterface
 {
     public partial class Form1 : Form
@@ -5,6 +7,39 @@ namespace ConceptsPOO.UserInterface
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            if (txtYear.Text.Length == 0)
+            {
+                MessageBox.Show("You must enter a year.", "Error");
+                return;
+            }
+            if (txtMonth.Text.Length==0)
+            {
+                MessageBox.Show("You must enter a month.", "Error");
+                return;
+            }
+            if (txtDay.Text.Length==0)
+            {
+                MessageBox.Show("You must enter a day.", "Error");
+                return;
+            }
+            try
+            {
+                int year = Convert.ToInt32(txtYear.Text);
+                int month = Convert.ToInt32(txtMonth.Text);
+                int day = Convert.ToInt32(txtDay.Text);
+
+                var date = new Date(year, month, day);
+                MessageBox.Show($"Congratulations the date: {date} is valid!", "Confirm");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Something wrong was ocurred: {ex.Message}", "Error");
+            }
         }
     }
 }
