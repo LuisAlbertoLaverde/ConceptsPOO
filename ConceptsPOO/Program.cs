736b32b60630e41a329dc167e1f6cf3fd947b177
Alberto <unumnio@gmail.com>
2023-06-29 16:03:59 -0500

    creating class library (dll-ConceptsPOO.Logic) and windows form project (ConceptsPOO.UserInterface)

--- ConceptsPOO/Program.cs
+++ ConceptsPOO/Program.cs
@@ -1,11 +1,19 @@
-﻿using ConceptsPOO;
+﻿
+using ConceptsPOO.Logic;
 
 try
 {
-    var date1 = new Date() { Year = 2028, Month = 2, Day = 29 };
-    var date2 = new Date() { Year = 1974, Month = 12, Day = 14 };
+    var date1 = new Date();
+    var date2 = new Date(2023,2,14);
+    var date3 = new Date()  
+    {
+        Year=2024, 
+        Month=2, 
+        Day=1 
+    };
     Console.WriteLine(date1);
     Console.WriteLine(date2);
+    Console.WriteLine(date3);
 
 }
 catch (Exception ex)
